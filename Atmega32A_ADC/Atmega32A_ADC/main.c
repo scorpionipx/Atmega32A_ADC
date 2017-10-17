8e88da9effc1610f50dcbed3a1ae19f51b1bb869
DESKTOP-PE8APUQ\ScorpionIPX <scorpionipx@gmail.com>
2017-10-17 20:52:44 +0300

    Created project

---
+++ Atmega32A_ADC/Atmega32A_ADC/main.c
@@ -0,0 +1,18 @@
+/*
+ * Atmega32A_ADC.c
+ *
+ * Created: 17-Oct-17 8:52:00 PM
+ * Author : ScorpionIPX
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
