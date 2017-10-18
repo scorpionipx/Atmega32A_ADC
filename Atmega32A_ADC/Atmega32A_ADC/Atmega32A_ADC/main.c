287d37b31c3b68349a7ca2fe61687cbb6cf79309
Danut Popa <scorpionipx@gmail.com>
2017-10-18 10:08:27 +0300

    Rebuild project

---
+++ Atmega32A_ADC/Atmega32A_ADC/Atmega32A_ADC/main.c
@@ -0,0 +1,19 @@
+/*
+ * Atmega32A_ADC.c
+ *
+ * Created: 18-Oct-17 10:07:11 AM
+ * Author : uidq6025
+ */ 
+
+#include <avr/io.h>
+#include "adc_driver.h"
+
+int main(void)
+{
+    ADC_init();
+	
+    while (1) 
+    {
+    }
+}
+
