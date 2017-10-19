69647bee769adf0a770d61a98b1cfca60d099b04
DESKTOP-PE8APUQ\ScorpionIPX <scorpionipx@gmail.com>
2017-10-19 19:18:45 +0300

    try to bkup

--- Atmega32A_ADC/Atmega32A_ADC/Atmega32A_ADC/main.c
+++ Atmega32A_ADC/Atmega32A_ADC/Atmega32A_ADC/main.c
@@ -39,7 +39,11 @@ int main(void)
 		adc_value = ADC_get_value(adc_channel);
 		
 		hx_set_coordinates((adc_channel / 4) * HX_MAX_COL / 2 + 18, (adc_channel % 4) + 3);
+<<<<<<< HEAD
 		hx_write_char(HX_ch_1);
+=======
+		hx_write_char(HX_ch_0);
+>>>>>>> parent of d11fbbf... Updated 8x6 characters
 		hx_write_char(HX_ch_0);
 		hx_write_char(HX_ch_0);
 		hx_write_char(HX_ch_0);
